ecd7f044491b2a269b26c49494984e0fe6b3cb7a
Chaitanya Vellanki <me@chaitanyavellanki.com>
2013-02-06 19:02:46 -0800

    Initial Commit

---
+++ magic_constants_reboot.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/reboot.h>
+
+int main() {
+  printf("MAGIC2 0x%X\n", LINUX_REBOOT_MAGIC2);
+  printf("MAGIC2A 0x%X\n", LINUX_REBOOT_MAGIC2A);
+  printf("MAGIC2B 0x%X\n", LINUX_REBOOT_MAGIC2B);
+  printf("MAGIC2C 0x%X\n", LINUX_REBOOT_MAGIC2C);
+  return 0;
+
+}
---
+++ version.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <gnu/libc-version.h>
+
+int main() {
+
+  printf("%s", gnu_get_libc_version());
+  return 0;
+
+}
