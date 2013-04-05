c06b4f09006d19991a15769245e39ed65db39c84
NagaChaitanya Vellanki <nvellanki@nvidia.com>
2013-04-05 16:28:14 -0700

    TRY/THROW/CATCH example

---
+++ exception_handling_1.c
@@ -0,0 +1,32 @@
+/*
+ * Author: NagaChaitanya Vellanki
+ *
+ * TRY/THROW/CATCH example
+ * Reference: http://www.di.unipi.it/~nids/docs/longjump_try_trow_catch.html
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+
+
+#define TRY do{ jmp_buf env; if(!setjmp(env)) {
+#define CATCH } else {
+#define END_TRY } }while(0)
+#define THROW longjmp(env, 1)
+
+int main(int argc, char *argv[]) {
+
+  TRY {
+    printf("In TRY statement\n");
+    THROW;
+    printf("not reachable\n");
+  }
+  CATCH {
+    printf("exception caught\n");
+  }
+  END_TRY;
+
+  exit(EXIT_SUCCESS);
+}
