b53a1a882746c27a0f5c8c11072ef60f5ce47e8f
NagaChaitanya Vellanki <nvellanki@nvidia.com>
2013-06-07 15:04:26 -0700

    Fix: err_exit function

--- set_ids_example.c
+++ set_ids_example.c
@@ -20,6 +20,8 @@ void err_exit(const char *format, ...) {
   va_start(args, format);
   vfprintf(stderr, format, args);
   va_end(args);
+
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[]) {
