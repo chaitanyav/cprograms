eb96f24d8496d1421889dae93f067acee1d8f068
NagaChaitanya Vellanki <me@chaitanyavellanki.com>
2013-06-16 08:54:04 -0700

    Add uname_example.c

---
+++ uname_example.c
@@ -0,0 +1,47 @@
+/*
+ * Author: NagaChaitanya Vellanki
+ *
+ *
+ *
+ * sysname -> /proc/sys/kernel/ostype
+ * version -> /proc/sys/kernel/version
+ * release -> /proc/sys/kernel/release
+ */
+
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/utsname.h>
+
+void err_exit(const char *format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+  struct utsname utsbuf;
+
+  if(uname(&utsbuf) != -1) {
+    printf("Kernel name: %s\n", utsbuf.sysname);
+    printf("Hostname: %s\n", utsbuf.nodename);
+    printf("Kernel Release: %s\n", utsbuf.release);
+    printf("Kernel version: %s\n", utsbuf.version);
+    printf("Machine : %s\n", utsbuf.machine);
+#ifdef _GNU_SOURCE
+    printf("NIS Domain name: %s\n", utsbuf.domainname);
+#endif
+
+  } else {
+    err_exit("Error on uname, %s\n", strerror(errno));
+  }
+
+  exit(EXIT_SUCCESS);
+}
