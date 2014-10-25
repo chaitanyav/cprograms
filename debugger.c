4cc431c15e7eaeeb64c80941fc22ce6c10ad5e3d
NagaChaitanya Vellanki <me@chaitanyavellanki.com>
2014-10-25 06:24:28 -0700

    Print floating point unit info

--- debugger.c
+++ debugger.c
@@ -3,72 +3,136 @@
 
 /* References:
  * 1. http://www.alexonlinux.com/how-debugger-works
- * 2. Free BSD Kernel source code
+ * 2. http://stereopsis.com/FPU.html
+ * 3. Free BSD Kernel source code
 */
 
 /* Sample Output
  * ---------------------------------------------------------------------------
  *
- * Parent pid is:1750 Child pid is:11257
- * launching the debugger
- * Pid is 11257
- * Parent pid is:11257 Child pid is:0
- * launching the debuggie ./loop, pid is 11258
- * Printing General Purpose x86 registers of the child process with pid: 11258
- * ---------------------------------------------------------------
- *  trapno: 0xfffff800
- *  ebp: 0x603750
- *  eip: 0x603750
- *  esp: 0xffffddc8
- *  esi: 0x53c1000
- *  edi: 0xfffff800
- *  eax: 0x1b
- *  ebx: 0x202
- *  ecx: 0x3b
- *  edx: 0xffffddc8
- *  cs:  0x43
- *  ss:  0x3b
- *  gs: 0x1b
- *  fs: 0
- *  ds: 0
- *  es: 0
- *  eflags: 0x202
- *  ---------------------------------------------------------------
- *  Printing all flags
- *  ---------------------------------------------------------------
- *  CF: 0
- *  RESERVED: 1
- *  PF: 0
- *  AF: 0
- *  ZF: 0
- *  SF: 0
- *  TF: 0
- *  IF: 1
- *  DF: 0
- *  OF: 0
- *  IOPL: 0
- *  NT: 0
- *  RF: 0
- *  VM: 0
- *  VM: 0
- *  VIF: 0
- *  VIP: 0
- *  ID: 0
- *  ---------------------------------------------------------------
- *  ---------------------------------------------------------------
- *  Printing Debug x86 registers of the child process with pid: 11258
- *  ---------------------------------------------------------------
- *  Debug Address 0:0x13
- *  Debug Address 1:0x3b
- *  Debug Address 2:0x3b
- *  Debug Address 3:0xffffddc8
- *  Reserved: 0
- *  Reserved: 0
- *  Debug Status: 0x2
- *  Debug Control: 0
- *  ---------------------------------------------------------------
- *  Child process was stopped due to signal Trace/BPT trap
- */
+        Parent pid is:2416 Child pid is:16355
+        launching the debugger
+        Pid is 16355
+        Parent pid is:16355 Child pid is:0
+        launching the debuggie ./loop, pid is 16356
+        Printing General Purpose x86 registers of the child process with pid: 16356
+        ---------------------------------------------------------------
+        trapno: 0xfffff800
+        ebp: 0x2804a660
+        eip: 0x2804a660
+        esp: 0xffffce10
+        esi: 0x2066a380
+        edi: 0xfffff800
+        eax: 0x1b
+        ebx: 0x202
+        ecx: 0x3b
+        edx: 0xffffce10
+        cs:  0x33
+        ss:  0x3b
+        gs: 0x1b
+        fs: 0
+        ds: 0
+        es: 0
+        eflags: 0x202
+        ---------------------------------------------------------------
+        Printing all flags
+        ---------------------------------------------------------------
+        CF: 0
+        RESERVED: 1
+        PF: 0
+        AF: 0
+        ZF: 0
+        SF: 0
+        TF: 0
+        IF: 1
+        DF: 0
+        OF: 0
+        IOPL: 0
+        NT: 0
+        RF: 0
+        VM: 0
+        VM: 0
+        VIF: 0
+        VIP: 0
+        ID: 0
+        ---------------------------------------------------------------
+        ---------------------------------------------------------------
+        Printing Debug x86 registers of the child process with pid: 16356
+        ---------------------------------------------------------------
+        Debug Address 0:0x13
+        Debug Address 1:0x3b
+        Debug Address 2:0x3b
+        Debug Address 3:0
+        Reserved: 0
+        Reserved: 0
+        Debug Status: 0x2
+        Debug Control: 0
+        ---------------------------------------------------------------
+
+        Floating point unit information
+        ---------------------------------------------------------------
+        Floating point environment information
+        ---------------------------------------------------------------
+        ---------------------------------------------------------------
+        Printing Floating Point Control Word
+        ---------------------------------------------------------------
+        IM (invalid operation mask): 1
+        DM (denormalized operand mask): 1
+        ZM (divide by zero mask): 1
+        OM (overflow mask): 1
+        UM (underflow mask): 1
+        PM (precision inexact mask): 1
+        Reserved: 0x1
+        PC (precision control): double precision
+        RC (rounding control): round to the nearest whole number
+        Status word: 0
+        Tag word: 0
+        Instruction Pointer: 0
+        Code segment selector: 0x33
+        opcode last executed: 0
+        operand offset: 0x3b
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 0
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 1
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 2
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 3
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 4
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 5
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 6
+        ---------------------------------------------------------------
+        ...
+        ---------------------------------------------------------------
+        Printing the contents of accumalator: 7
+        ---------------------------------------------------------------
+        ....
+        ---------------------------------------------------------------
+         Status Word
+        ---------------------------------------------------------------
+        0
+        ---------------------------------------------------------------
+         Padding
+        ---------------------------------------------------------------
+        ....
+        Child process was stopped due to signal Trace/BPT trap
+*/
 
 #include <stdio.h>
 #include <sys/ptrace.h>
@@ -81,36 +145,117 @@
 #include <errno.h>
 #include <string.h>
 
+void print_fpcontrol_word(uint32_t cw) {
+  printf("---------------------------------------------------------------\n");
+  printf("Printing Floating Point Control Word\n");
+  printf("---------------------------------------------------------------\n");
+  printf("IM (invalid operation mask): %d\n", (cw & 1) ? 1 : 0);
+  printf("DM (denormalized operand mask): %d\n", ((cw >> 1) & 1) ? 1 : 0);
+  printf("ZM (divide by zero mask): %d\n", ((cw >> 2) & 1) ? 1 : 0);
+  printf("OM (overflow mask): %d\n", ((cw >> 3) & 1) ? 1 : 0);
+  printf("UM (underflow mask): %d\n", ((cw >> 4) & 1) ? 1 : 0);
+  printf("PM (precision inexact mask): %d\n", ((cw >> 5) & 1) ? 1 : 0);
+  printf("Reserved: %#x\n", ((cw >> 6) & 0x3));
+
+  printf("PC (precision control): ");
+  switch((cw >> 8) & 0x3) {
+    case 0:
+      printf("single precision\n");
+      break;
+    case 1:
+      printf("reserved\n");
+      break;
+    case 2:
+      printf("double precision\n");
+      break;
+    case 3:
+      printf("extended precision\n");
+      break;
+  }
+
+  printf("RC (rounding control): ");
+  switch((cw >> 10) & 0x3) {
+    case 0:
+      printf("round to the nearest whole number\n");
+      break;
+    case 1:
+      printf("round down toward - infinity\n");
+      break;
+    case 2:
+      printf("round up toward + infinity\n");
+      break;
+    case 3:
+      printf("round toward zero (truncate)\n");
+      break;
+  }
+}
+
+void print_fpregisters(struct fpreg fp_reg_info, pid_t child) {
+  printf("\nFloating point unit information\n");
+  printf("---------------------------------------------------------------\n");
+  printf("Floating point environment information\n");
+  printf("---------------------------------------------------------------\n");
+  print_fpcontrol_word(fp_reg_info.fpr_env[0]);
+  printf("Status word: %#x\n", fp_reg_info.fpr_env[1]);
+  printf("Tag word: %#x\n", fp_reg_info.fpr_env[2]);
+  printf("Instruction Pointer: %#x\n", fp_reg_info.fpr_env[3]);
+  printf("Code segment selector: %#x\n", fp_reg_info.fpr_env[4]);
+  printf("opcode last executed: %#x\n", fp_reg_info.fpr_env[5]);
+  printf("operand offset: %#x\n", fp_reg_info.fpr_env[6]);
+
+  for(int acc = 0; acc < 8; acc++) {
+    printf("---------------------------------------------------------------\n");
+    printf("Printing the contents of accumalator: %d\n", acc);
+    printf("---------------------------------------------------------------\n");
+    for(int i = 0; i < 10; i++) {
+      printf("%#x\n", fp_reg_info.fpr_acc[acc][i]);
+    }
+  }
+
+
+  printf("---------------------------------------------------------------\n");
+  printf(" Status Word\n");
+  printf("---------------------------------------------------------------\n");
+  printf("%#x\n", fp_reg_info.fpr_ex_sw);
+
+
+  printf("---------------------------------------------------------------\n");
+  printf(" Padding\n");
+  printf("---------------------------------------------------------------\n");
+  for(int i = 0; i < 64; i++) {
+    printf("%#x\n", fp_reg_info.fpr_pad[i]);
+  }
+}
 
 void print_eflags(uint32_t eflags) {
-    fprintf(stdout, "---------------------------------------------------------------\n");
-    fprintf(stdout, "Printing all flags\n");
-    fprintf(stdout, "---------------------------------------------------------------\n");
-    fprintf(stdout, "CF: %d\n", (eflags & 1) ? 1 : 0);
-    fprintf(stdout, "RESERVED: %d\n", ((eflags >> 1) & 1) ? 1 : 0);
-    fprintf(stdout, "PF: %d\n", ((eflags >> 2) & 1) ? 1 : 0);
-    fprintf(stdout, "AF: %d\n", ((eflags >> 4) & 1) ? 1 : 0);
-    fprintf(stdout, "ZF: %d\n", ((eflags >> 6) & 1) ? 1 : 0);
-    fprintf(stdout, "SF: %d\n", ((eflags >> 7) & 1) ? 1 : 0);
-    fprintf(stdout, "TF: %d\n", ((eflags >> 8) & 1) ? 1 : 0);
-    fprintf(stdout, "IF: %d\n", ((eflags >> 9) & 1) ? 1 : 0);
-    fprintf(stdout, "DF: %d\n", ((eflags >> 10) & 1) ? 1 : 0);
-    fprintf(stdout, "OF: %d\n", ((eflags >> 11) & 1) ? 1 : 0);
-    fprintf(stdout, "IOPL: %#x\n", (eflags >> 12) & 3);
-    fprintf(stdout, "NT: %d\n", ((eflags >> 14) & 1) ? 1 : 0);
-    fprintf(stdout, "RF: %d\n", ((eflags >> 16) & 1) ? 1 : 0);
-    fprintf(stdout, "VM: %d\n", ((eflags >> 17) & 1) ? 1 : 0);
-    fprintf(stdout, "VM: %d\n", ((eflags >> 18) & 1) ? 1 : 0);
-    fprintf(stdout, "VIF: %d\n", ((eflags >> 19) & 1) ? 1 : 0);
-    fprintf(stdout, "VIP: %d\n", ((eflags >> 20) & 1) ? 1 : 0);
-    fprintf(stdout, "ID: %d\n", ((eflags >> 21) & 1) ? 1 : 0);
-    fprintf(stdout, "---------------------------------------------------------------\n");
+    printf("---------------------------------------------------------------\n");
+    printf("Printing all flags\n");
+    printf("---------------------------------------------------------------\n");
+    printf("CF: %d\n", (eflags & 1) ? 1 : 0);
+    printf("RESERVED: %d\n", ((eflags >> 1) & 1) ? 1 : 0);
+    printf("PF: %d\n", ((eflags >> 2) & 1) ? 1 : 0);
+    printf("AF: %d\n", ((eflags >> 4) & 1) ? 1 : 0);
+    printf("ZF: %d\n", ((eflags >> 6) & 1) ? 1 : 0);
+    printf("SF: %d\n", ((eflags >> 7) & 1) ? 1 : 0);
+    printf("TF: %d\n", ((eflags >> 8) & 1) ? 1 : 0);
+    printf("IF: %d\n", ((eflags >> 9) & 1) ? 1 : 0);
+    printf("DF: %d\n", ((eflags >> 10) & 1) ? 1 : 0);
+    printf("OF: %d\n", ((eflags >> 11) & 1) ? 1 : 0);
+    printf("IOPL: %#x\n", (eflags >> 12) & 3);
+    printf("NT: %d\n", ((eflags >> 14) & 1) ? 1 : 0);
+    printf("RF: %d\n", ((eflags >> 16) & 1) ? 1 : 0);
+    printf("VM: %d\n", ((eflags >> 17) & 1) ? 1 : 0);
+    printf("VM: %d\n", ((eflags >> 18) & 1) ? 1 : 0);
+    printf("VIF: %d\n", ((eflags >> 19) & 1) ? 1 : 0);
+    printf("VIP: %d\n", ((eflags >> 20) & 1) ? 1 : 0);
+    printf("ID: %d\n", ((eflags >> 21) & 1) ? 1 : 0);
+    printf("---------------------------------------------------------------\n");
 }
 
 void print_gpregisters(struct reg reg_info, pid_t child) {
-  fprintf(stdout, "Printing General Purpose x86 registers of the child process with pid: %u\n", child);
-  fprintf(stdout, "---------------------------------------------------------------\n");
-  fprintf(stdout,
+  printf("Printing General Purpose x86 registers of the child process with pid: %u\n", child);
+  printf("---------------------------------------------------------------\n");
+  printf(
       "trapno: %#x\n"
       "ebp: %#x\n"
       "eip: %#x\n"
@@ -146,40 +291,40 @@ void print_gpregisters(struct reg reg_info, pid_t child) {
       reg_info.r_es,
       reg_info.r_eflags);
       print_eflags(reg_info.r_eflags);
-  fprintf(stdout, "---------------------------------------------------------------\n");
+  printf("---------------------------------------------------------------\n");
 }
 
 void print_dbgregisters(struct dbreg debug_reg_info, pid_t child) {
-  fprintf(stdout, "Printing Debug x86 registers of the child process with pid: %u\n", child);
-  fprintf(stdout, "---------------------------------------------------------------\n");
+  printf("Printing Debug x86 registers of the child process with pid: %u\n", child);
+  printf("---------------------------------------------------------------\n");
   for(int i = 0; i < 8; i++) {
     switch(i) {
       case 0:
       case 1:
       case 2:
       case 3:
-          fprintf(stdout, "Debug Address %d:", i);
+          printf("Debug Address %d:", i);
           break;
       case 4:
-      case 5: fprintf(stdout, "Reserved: ");
+      case 5: printf("Reserved: ");
               break;
-      case 6: fprintf(stdout, "Debug Status: ");
+      case 6: printf("Debug Status: ");
               break;
-      case 7: fprintf(stdout, "Debug Control: ");
+      case 7: printf("Debug Control: ");
               break;
     }
-    fprintf(stdout, "%#x\n", debug_reg_info.dr[i]);
+    printf("%#x\n", debug_reg_info.dr[i]);
   }
 
-  fprintf(stdout, "---------------------------------------------------------------\n");
+  printf("---------------------------------------------------------------\n");
 }
 
 void handle_child_signal(int signal) {
-  fprintf(stdout, "I am in the %s handler, pid is %u\n", strsignal(signal), getpid());
+  printf("I am in the %s handler, pid is %u\n", strsignal(signal), getpid());
 }
 
 void launch_debuggie(char *name) {
-  fprintf(stdout, "launching the debuggie %s, pid is %u\n", name, getpid());
+  printf("launching the debuggie %s, pid is %u\n", name, getpid());
   if(ptrace(PT_TRACE_ME, 0, NULL, 0)) {
     fprintf(stderr, "Error on ptrace code is %d, reason is %s\n", errno, strerror(errno));
     exit(EXIT_FAILURE);
@@ -193,8 +338,8 @@ void launch_debuggie(char *name) {
 void launch_debugger() {
   int status;
   pid_t child;
-  fprintf(stdout, "launching the debugger\n");
-  fprintf(stdout, "Pid is %u\n", getpid());
+  printf("launching the debugger\n");
+  printf("Pid is %u\n", getpid());
   if(signal(SIGCHLD, handle_child_signal) == SIG_ERR) {
     fprintf(stderr, "Error on signal call\n");
     exit(EXIT_FAILURE);
@@ -202,17 +347,23 @@ void launch_debugger() {
 
   do {
     child = wait(&status);
+
     struct reg reg_info;
     struct dbreg debug_reg_info;
+    struct fpreg fp_reg_info;
+
     ptrace(PT_GETREGS, child, (caddr_t ) &reg_info, 0);
     ptrace(PT_GETREGS, child, (caddr_t ) &debug_reg_info, 0);
+    ptrace(PT_GETFPREGS, child, (caddr_t ) &fp_reg_info, 0);
     print_gpregisters(reg_info, child);
     print_dbgregisters(debug_reg_info, child);
+    print_fpregisters(fp_reg_info, child);
+
     if(WIFSTOPPED(status)) {
-      fprintf(stdout, "Child process was stopped due to signal %s\n", strsignal(WSTOPSIG(status)));
+      printf("Child process was stopped due to signal %s\n", strsignal(WSTOPSIG(status)));
     }
     if(WIFSIGNALED(status)) {
-      fprintf(stdout, "Child %u received signal %s\n", child, strsignal(WTERMSIG(status)));
+      printf("Child %u received signal %s\n", child, strsignal(WTERMSIG(status)));
     }
   }while(!WIFEXITED(status));
 }
@@ -228,10 +379,10 @@ int main(int argc, char *argv[]) {
 
   child = fork();
   if(child == 0) {
-    fprintf(stdout, "Parent pid is:%u Child pid is:%u\n", getppid(), child);
+    printf("Parent pid is:%u Child pid is:%u\n", getppid(), child);
     launch_debuggie(argv[1]);
   } else if(child > 0) {
-    fprintf(stdout, "Parent pid is:%u Child pid is:%u\n", getppid(), getpid());
+    printf("Parent pid is:%u Child pid is:%u\n", getppid(), getpid());
     launch_debugger();
   } else {
     fprintf(stderr, "Error on fork %d, reason %s\n", errno, strerror(errno));
